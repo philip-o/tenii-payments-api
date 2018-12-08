da57b7772b81b6a10f217ece0a516dde0be06ff7
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-12-08 18:55:16 +0000

    Added new ping route

--- src/main/scala/com/ogunleye/tenii/payments/TeniiPaymentsApp.scala
+++ src/main/scala/com/ogunleye/tenii/payments/TeniiPaymentsApp.scala
@@ -5,7 +5,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.RouteConcatenation
 import akka.pattern.CircuitBreaker
 import akka.stream.ActorMaterializer
-import com.ogunleye.tenii.payments.routes.{PaymentRoute, PotRoute}
+import com.ogunleye.tenii.payments.routes.{PaymentRoute, PingRoute, PotRoute}
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.duration._
@@ -26,8 +26,9 @@ object TeniiApp extends App with LazyLogging with RouteConcatenation {
   //val swaggerDocRoute = new SwaggerDocRoute().routes
   val paymentRoute = new PaymentRoute().route
   val potRoute = new PotRoute().route
+  val pingRoute = new PingRoute().route
 
-  val routes = paymentRoute ~ potRoute
+  val routes = paymentRoute ~ potRoute ~ pingRoute
 
   val port = Properties.envOrElse("PORT", "8080").toInt
   Http().bindAndHandle(routes, "0.0.0.0", port)
